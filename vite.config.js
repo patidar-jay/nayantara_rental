bb971c58e3f2564221d455571bbf94336cd9fb71
Jaydeep Patidar <jaypatelpatidarm51@gmail.com>
2026-06-07 02:22:08 +0530

    fix: Vercel build TS error by adding @types/node and fixing tsconfig

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@core': path.resolve(__dirname, './src/core'),
+            '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
+            '@presentation': path.resolve(__dirname, './src/presentation'),
+            '@hooks': path.resolve(__dirname, './src/presentation/hooks'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+        },
+    },
+});
